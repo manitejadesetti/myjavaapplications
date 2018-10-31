84bbd025eed50123884ca1b129f9f0c5dd1b97a8
manitejadesetti <maniteja.d@kustommade.com>
2018-10-31 19:40:52 +0530

    moving to repo

---
+++ SpringBoot/src/main/java/core/HelloController.java
@@ -0,0 +1,17 @@
+package core;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/welcome")
+	public String sayHello() {
+		return "Hello Spring Boot";
+	}
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello";
+	}
+
+}
